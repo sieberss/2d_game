6a0ca3a38e91cdfa9a1c306a50c9089de30271bd
Nora Kauczor <n.kauczor@web.de>
2024-08-20 12:16:26 +0200

    test move method succesfully

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -2,9 +2,20 @@ package org.example;
 
 public class PlayerCharacter {
 
+    static int x = 0;
+    static int y = 0;
 
     public static int getX() {
         return 0;
+    }
+
+    public static int getY() {
+        return 0;
+    }
+
+    public static void move(char userInput) {
+        if (userInput == 'W') {y++;};
+
     }
 
 }
--- src/test/java/org/example/PlayerCharacterTest.java
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -8,9 +8,31 @@ class PlayerCharacterTest {
 
     @Test
     void getX_returns0() {
+        PlayerCharacter.x = 0;
         int expected = 0;
         int actual = PlayerCharacter.getX();
         assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void getY_returns0() {
+
+        int expected = 0;
+        int actual = PlayerCharacter.getY();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void move_yShouldBe1_whenCalledWithW() {
+        char userInput = 'W';
+        int expected = 1;
+        PlayerCharacter.move(userInput); // move hat keinen return, wird nur ausgeführt
+        int actual = PlayerCharacter.y;
+        assertEquals(expected, actual);
+
+
     }
 
 }
